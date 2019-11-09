aa5a5e08d03c64bab9d55b9c3de01917a2bb49a9
stanleyycheung <stanleyycheung@gmail.com>
2019-11-09 16:05:26 +0000

    update

--- script.js
+++ script.js
@@ -50,26 +50,28 @@ function openBubble(position, text) {
     }
 }
 
-// function placesSearch(platform) {
-//     var placesService = platform.getPlacesService(),
-//         parameters = {
-//             at: '51.5083,-0.1256',
-//             cat: 'accommodation'
-//         };
-//
-//     placesService.explore(parameters,
-//         function(result) {
-//             console.log(result.results.items);
-//             for (i = 0; i < result.results.items.length; i += 1) {
-//                 console.log(result.results.items[i])
-//             }
-//         },
-//         function(error) {
-//             console.log(error);
-//         });
-// }
-// placesSearch(platform);
 
-d3.csv("worldcities.csv").then(function(data) {
-  console.log(data[0]);
+d3.csv("/worldcities.csv").then(function(data) {
+    for (i = 0; i < 5; i += 1) {
+        // function placesSearch(platform) {
+        //     var placesService = platform.getPlacesService(),
+        //         parameters = {
+        //             at: '51.5083,-0.1256',
+        //             cat: 'accommodation'
+        //         };
+        //
+        //     placesService.explore(parameters,
+        //         function(result) {
+        //             console.log(result.results.items);
+        //             for (i = 0; i < result.results.items.length; i += 1) {
+        //                 console.log(result.results.items[i])
+        //             }
+        //         },
+        //         function(error) {
+        //             console.log(error);
+        //         });
+        // }
+        // placesSearch(platform);
+        console.log(data[i])
+    }
 });
